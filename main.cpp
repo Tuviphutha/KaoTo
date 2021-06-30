19adc3b37a28649253207825960b65e9e8f30bf5
KaoTo <67577706+Tuviphutha@users.noreply.github.com>
2021-06-30 13:33:00 +0700

    Phân Tích Thành Thừa Số Nguyên Tố
    Đưa ra cơ số và số mũ ở hai mảng khác nhau

--- main.cpp
+++ main.cpp
@@ -2,110 +2,76 @@
 #include <stdio.h>
 #include <math.h>
 using namespace std;
-int n,a[100],z,e;
-/////////////////////////////////////////////
-long ktmang (int a[100],int j)
+long n,i,a[100],b[100];
+long ktnt (long n)
 {
- for (long i=0;i<j;i++)
-    {
-        long d=a[i];
-    for (long x=i+1;x<j;x++)
-    {
-        if (a[x]==d)
-            {
-                a[x]=0;
-            }
-    }
-    }
+    long d=1;
     long dem=0;
-    for (long i=0;i<=j;i++)
+    if (n<2) d=0;
+    for (long i=2;i<=sqrt(n);i++)
     {
-        if (a[i]!=0) dem++;
-}
-return dem;
-}
-//////////////////////////////////////////////
-long ucln(int a, int b)
-{
-     while (b>0)
-    {
-        long r=a%b;
-        a=b;
-        b=r;
-    }
-  return a;
-}
-////////////////////////////////////////////////
-long ktnt (int n)
-{
-  long d=1;
-  if(n<2)
-    {
-     d=0;
-    }
-  long dem=0;
-    for(int i=2;i<=sqrt(n);i++)
-    {
-        if(n % i == 0)
-        {
-            dem++;
-        }
+        if (n%i==0) dem++;
     }
     if (dem==0) d=1; else d=0;
     return d;
+
 }
-/////////////////////////////////////////
 int main()
 {
-   freopen("KTcarmichael.inp","r",stdin);
-   freopen("KTcarmichael.out","w",stdout);
-   cin>>n;
-   if (ktnt(n)==1)
-   {
-       cout<<"ko là carmichael";
-   }
-else
+    freopen("phantichrathuasonguyento.inp","r",stdin);
+    freopen("phantichrathuasonguyento.out","w",stdout);
+    long j=0,c=0;
+cin>>n;
+long m=n,dem=0;
+for (long i=2;i<=m;i++)
 {
-    long j=0,dem=0;
-    long m=n;
-    for (long i=2;i<=m;i++)
-{
-         if ((ktnt(i)==1))
-       {
-           while (n%i==0)
-          {
-            j=j+1;
-            a[j]=i;
+     if (ktnt(i)==1);
+    {
+        while (n%i==0)
+        {
+            j++;
             //cout<<i<<endl;
+            a[j]=i;
             n=n/i;
-          }
-       }
+            dem++;
+            //cout<<dem<<endl;
+        }
+        if (dem!=0)
+        {
+           c++;
+           b[c]=dem;
+        }
+    }
+    dem=0;
     if (n==1) break;
 }
-  if (j==0)
-    cout<<"ko là so carmichael";
-  else
+cout<<"co so:"<<" ";
+//cout<<c<<endl;
+for (i=1;i<=j;i++)
 {
-    z=j;
-    e=ktmang(a,j);
-    if ((e!=j)or(e<3))
+    for (long x=0;x<i;x++)
+    {
+        if (a[i]==a[x])
         {
-            cout<<"ko là so carmichael";
-            return 0;
+            for (long k=i;k<=j;k++)
+            {
+                a[k]=a[k+1];
+                j--;
+                i--;
+            }
         }
-    else
-   {
-        for (long i=1;i<=j;i++)
-         {
-             long thg=(m-1)%(a[i]-1);
-             if (thg==0)
-             {
-              dem++;
-             }
-        }
-     if (dem==j) cout<<"co la so carmichael"; else cout<<"ko là so carmichael";
     }
 }
+for (long o=1;o<=j;o++)
+{
+ cout<<a[o]<<" ";
+}
+cout<<endl;
+cout<<"so mu:"<<" ";
+for (long o=1;o<=c;o++)
+{
+ //if (b[o]!=0)
+    cout<<b[o]<<" ";
 }
     return 0;
 }
